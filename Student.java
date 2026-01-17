92ab69a8ee54702d6f983be88b9128835b90f325
shakthi <shakthisravanth001@gmail.com>
2026-01-17 10:47:33 +0530

    Lab - 1 : Initial commit

---
+++ Student.java
@@ -0,0 +1,10 @@
+    
+    // Create a class named Student
+
+    // Declare an int variable to store student ID
+
+    // Declare a String variable to store student name
+
+    // Declare an int variable to store student age
+
+    // Declare a String variable to store course name
---
+++ StudentApp.java
@@ -0,0 +1,21 @@
+// Create a class named StudentApp
+
+// Inside the class, create the main method
+
+// Create an object of the Student class
+
+// Assign values to the student object for:
+
+// id, name, age, and course
+
+// Print a heading like "Student Details"
+
+// Print a line separator
+
+// Display student id using the object
+
+// Display student name using the object
+
+// Display student age using the object
+
+// Display student course using the object
